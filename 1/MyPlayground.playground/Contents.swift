55ad3ec96beb336d5168988e82a6d420349a66d9
ShivaSMBP <shivajayaramireddy@gmail.com>
2023-05-31 14:27:15 -0400

    new repo

---
+++ 1/MyPlayground.playground/Contents.swift
@@ -0,0 +1,15 @@
+/*
+Primite data types
+*/
+
+let dt_bool: Bool
+//print(dt_bool) // this is wrong, there is no default initiation
+dt_bool = false
+
+
+// Arrays
+
+let letters = ["a", "b", "c"]
+
+print(letters)
+
