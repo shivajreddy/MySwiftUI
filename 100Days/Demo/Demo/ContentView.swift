c52cb375a8b4399fe761709b162419de4da4ec98
Shiva Reddy <smbp@SMBP.local>
2023-07-05 23:20:34 -0400

    swiftui .

--- 100Days/Demo/Demo/ContentView.swift
+++ 100Days/Demo/Demo/ContentView.swift
@@ -1,10 +1,3 @@
-//
-//  ContentView.swift
-//  Demo
-//
-//  Created by Shiva Reddy on 7/4/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
@@ -20,8 +13,21 @@ struct ContentView: View {
     }
 }
 
+struct MyView: View{
+    var body: some View{
+        VStack{
+            RoundedRectangle(cornerRadius: 10)
+                .fill(Color(.red))
+                .frame(width: 200, height: 200)
+        }
+    }
+    
+}
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+//        ContentView()
+        MyView()
     }
 }
