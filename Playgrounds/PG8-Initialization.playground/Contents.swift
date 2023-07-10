b98db3e919bb7393d18c81a7a9cee3e755b866fa
Shiva Reddy <shivajreddy@outlook.com>
2023-07-10 16:23:12 -0400

    memory game, getting started

--- Playgrounds/PG8-Initialization.playground/Contents.swift
+++ Playgrounds/PG8-Initialization.playground/Contents.swift
@@ -3,6 +3,7 @@
 /// Customizing Initialization
 /// Default Initializers
 /// Initializer Delegation for Value Types
+
 /// Class Inheritance and Initialization
 /// Failable Initializers
 /// Required Initializers
@@ -10,16 +11,7 @@
 
 
 
-class SurveyQuestion {
-    let text: String
-    var response: String?
-    init(text: String) {
-        self.text = text
-    }
-    func ask() {
-        print(text)
-    }
-}
-let beetsQuestion = SurveyQuestion(text: "How about beets?")
-beetsQuestion.ask() // "How about beets?"
-beetsQuestion.response = "I also like beets. (But not with cheese.)"
+// Class Inheritance and Initialization
+
+
+
