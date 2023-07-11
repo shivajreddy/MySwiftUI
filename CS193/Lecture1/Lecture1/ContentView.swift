43fdc834c67808364fdf35952691b7736af99dad
Shiva Reddy <shivajreddy@outlook.com>
2023-07-10 20:17:59 -0400

    memory game basic card done

--- CS193/Lecture1/Lecture1/ContentView.swift
+++ CS193/Lecture1/Lecture1/ContentView.swift
@@ -1,54 +1,84 @@
 import SwiftUI
 
+
+/// THEME
+var theme1: [String] = ["🐯", "🐶", "🐭", "🐒", "🦞", "🪼", "🐥", "🪲", "🦚", "🐖", "🦜", "🐠", "🦄", "🐨", "🐼", "🦁"]
+var theme2: [String] = ["🫒", "🌶️", "🥑", "🍉", "🍐", "🍎", "🫐", "🍒", "🍑", "🍍", "🥥", "🧀", "🍠", "🍖", "🍔", "🥩"]
+
 /// Card that shows image on front, default back of that card
 struct MyCard: View {
     @State var count = 0
     @State var isUp = false
+    var content: String = "🚀"
     //    @State var size: CardSize = CardSize(width: 50, height: 50)
     
-    var width: CGFloat { 50 }
-    var height: CGFloat { 50 }
+    var width: CGFloat { 60 }
+    var height: CGFloat { 100 }
+    
+    func handleClick() {
+        // Flip the card        isUp = !isUp
+        print("Count = \(count). Card isUp: \(isUp)")
+        count += 1
+    }
     
     var body: some View {
         ZStack{
-            Rectangle().fill(Color.cyan)
-            Text("🚀")
+            Rectangle().fill( isUp ? Color.white : Color.red)
+            Text(isUp ? content : "")
         }
+        .overlay( /// apply a rounded border
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(.blue, lineWidth: 8))
         .frame(maxWidth: width, maxHeight: height)
-        .cornerRadius(12)
-        .onTapGesture {
-            print("Count = \(count)")
-            count += 1
-        }
+        .cornerRadius(20)
+        .onTapGesture { handleClick() }
     }
 }
 
-func updateGrid(){
-}
-
-struct GameView: View {
+struct GameFormView: View {
+    
+    func updateGrid(){
+        print("Choosen grid size: \(gridSize + 5)")
+    }
     
     /// min-grid size = 5 (index = 0), max-grid size = 10(index=5)
     @State private var gridSize: Int = 0
-    
     var body: some View {
         VStack(){
-            Form{
-                VStack {
-                    Picker("choose grid size:", selection: $gridSize) {
+            VStack {
+                HStack{
+                    Text("Grid:")
+                    Picker("", selection: $gridSize) {
                         ForEach(5..<11){num in Text("\(num)") }
                     }
                     .pickerStyle(MenuPickerStyle())
-                    Button("Update Grid") {
-                        print("Choosen grid size: \(gridSize + 5)")
-                    }
+                    Button("Update Grid") { updateGrid() }
                 }
             }
             .frame(maxHeight: 150)
             .padding(.horizontal)
             .alignmentGuide(.top) { _ in CGFloat(200)}
-            MyCard()
-            MyCard()
+        }
+    }
+}
+
+struct GameView: View {
+    
+    var body: some View {
+        VStack{
+            GameFormView()
+            HStack{
+                MyCard()
+                MyCard()
+                MyCard()
+                MyCard()
+                MyCard()
+                MyCard()
+                MyCard()
+                MyCard()
+                MyCard()
+                MyCard()
+            }
         }
     }
 }
--- Playgrounds/PG8-Initialization.playground/Contents.swift
+++ Playgrounds/PG8-Initialization.playground/Contents.swift
@@ -12,6 +12,43 @@
 
 
 // Class Inheritance and Initialization
+/// https://docs.swift.org/swift-book/documentation/the-swift-programming-language/initialization
 
+// All class's stored ppties must have an initial value
 
+/// Swift defines two kinds of initializers for class types to help ensure all stored properties receive an initial value. These are known as designated initializers and convenience initializers.
+
+struct Human {
+    var age: Int
+}
+
+class Person {
+    var age: Int    // ERROR: all class-type ppty's must me initialized
+    var name: String
+    
+    /// Delegated Initializer
+    init(age: Int, name: String) {
+        self.age = age
+        self.name = name
+    }
+    /// Convenience Initializers
+    convenience init(age: Int) {
+        self.init(age: age, name: "rando")
+    }
+    
+    convenience init() {
+        self.init(age: -1, name: "rando -1" )
+    }
+    
+    func details(){
+        print("Person{age:\(age) name:\(name)}")
+    }
+}
+
+var p1 = Person(age: 21, name: "boy")
+p1.details()
+var p2 = Person(age: 27)
+p2.details()
+var p3 = Person()
+p3.details()
 
